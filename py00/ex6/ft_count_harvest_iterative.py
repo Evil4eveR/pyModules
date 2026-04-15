5bde349c48fea26a89ccd5382529a82ce2beaab7
Yassin Marmoud <ymarmoud@c2r1s2.42wolfsburg.de>
2026-04-15 19:22:21 +0200

    remove the header, flake8 check

--- py00/ex6/ft_count_harvest_iterative.py
+++ py00/ex6/ft_count_harvest_iterative.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_iterative.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: ymarmoud <ymarmoud@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/04/09 16:32:01 by ymarmoud          #+#    #+#              #
-#    Updated: 2026/04/09 16:33:43 by ymarmoud         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_count_harvest_iterative():
     days = input("Days until harvest:")
     for i in range(int(days)):
--- py00/ex7/ft_seed_inventory.py
+++ py00/ex7/ft_seed_inventory.py
@@ -1,18 +1,6 @@
-# **************************************************************************** #
-#																			  #
-#														 :::	  ::::::::	#
-#	ft_seed_inventory.py							   :+:	  :+:	:+:	#
-#													 +:+ +:+		 +:+	  #
-#	By: ymarmoud <ymarmoud@student.42.fr>		  +#+  +:+	   +#+		 #
-#												 +#+#+#+#+#+   +#+			#
-#	Created: 2026/04/09 16:37:14 by ymarmoud		  #+#	#+#			  #
-#	Updated: 2026/04/09 20:43:57 by ymarmoud		 ###   ########.fr		#
-#																			  #
-# **************************************************************************** #
-
 def	ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
 	if unit == "area":
-		print(f"L{seed_type} seeds: covers {quantity} square meters")
+		print(f"{seed_type}.capitalize() seeds: covers {quantity} square meters")
 	else:
 		if unit == "packets":
 			unit += " available"
@@ -20,4 +8,4 @@ def	ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
 			unit += " total"
 		else:
 			unit = "unknown"
-		print(f"{seed_type} seeds: {quantity} {unit}")
+		print(f"{seed_type}.capitalize() seeds: {quantity} {unit}")
