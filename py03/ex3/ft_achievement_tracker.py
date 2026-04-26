27e0809a3d6434fff41f474f02b3cca2206f986b
ezer0 <ymarmoud@gmail.com>
2026-04-26 23:32:44 +0200

    create players sets

--- py03/ex3/ft_achievement_tracker.py
+++ py03/ex3/ft_achievement_tracker.py
@@ -1,12 +1,28 @@
-
-
-
 def gen_player_achievements():
     pass
 
 
 def main() -> None:
-    pass
+    print("=== Achievement Tracker System ===")
+    alice = {"first_kill", "level_10", "treasure_hunter", "speed_demon","MVP"}
+    bob = {"first_kill", "level_10", "boss_slayer", "collector","MVP"}
+    charlie = {
+        "level_10",
+        "MVP",
+        "treasure_hunter",
+        "boss_slayer",
+        "speed_demon",
+        "perfectionist",
+    }
+    Dylan = {'Strategist',"MVP",'Speed Runner', 'Unstoppable', 'Untouchable', 'Boss Slayer'}
+    print("Player Alice: ", alice)
+    print("Player Bob: ", bob)
+    print("Player Charlie: ", charlie)
+    print("Player Dylan: ", Dylan)
+    union = Dylan | alice | bob | charlie
+    print("\nAll distinct achievements: ", union)
+    intersection = alice & bob & charlie & Dylan
+    print("\nCommon achievements:", intersection)
 
 if __name__ == "__main__":
     main()
