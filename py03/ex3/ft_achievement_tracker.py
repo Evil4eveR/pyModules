523dde0cca51698fcc328a2cb9cec9abdc268579
Yassin Marmoud <ymarmoud@c2r1s2.42wolfsburg.de>
2026-04-29 18:21:40 +0200

    ex3 solved

--- py03/ex3/ft_achievement_tracker.py
+++ py03/ex3/ft_achievement_tracker.py
@@ -1,28 +1,63 @@
-def gen_player_achievements():
-    pass
+import random
+
+
+def gen_player_achievements() -> set:
+    achievements = [
+        "first_kill", "level_10", "treasure_hunter", 
+        "speed_demon", "MVP", "Unstoppable", "boss_slayer",
+        "Strategist", "Speed_Runner", "Untouchable", "perfectionist",
+        "collector", "Legendary", "Deadeye", "Explorer", "VIP"
+        ]
+    random_number = random.randint(3, 8)
+    player_set = random.sample(achievements, k=random_number)
+    return set(player_set)
+
+
+def uniq_achiev(others: set, plyer: set) -> set:
+    lst = list(plyer)
+    for l1 in plyer:
+        if l1 in others:
+            lst.remove(l1)
+    return set(lst)
+
+
+def no_achiev(others: set, plyer: set) -> set:
+    lst = list(others)
+    for l1 in plyer:
+        if l1 in others:
+            lst.remove(l1)
+    return set(lst)
 
 
 def main() -> None:
-    print("=== Achievement Tracker System ===")
-    alice = {"first_kill", "level_10", "treasure_hunter", "speed_demon","MVP"}
-    bob = {"first_kill", "level_10", "boss_slayer", "collector","MVP"}
-    charlie = {
-        "level_10",
-        "MVP",
-        "treasure_hunter",
-        "boss_slayer",
-        "speed_demon",
-        "perfectionist",
-    }
-    Dylan = {'Strategist',"MVP",'Speed Runner', 'Unstoppable', 'Untouchable', 'Boss Slayer'}
+    print("=== Achievement Tracker System ===\n")
+    # players_list = ["alice", "bob", "charlie", "dylan"]
+    # for ply in players_list:
+    #     print(f"Player {ply}: {gen_player_achievements()}")
+
+    alice = gen_player_achievements()
+    bob = gen_player_achievements()
+    charlie = gen_player_achievements()
+    dylan = gen_player_achievements()
     print("Player Alice: ", alice)
     print("Player Bob: ", bob)
     print("Player Charlie: ", charlie)
-    print("Player Dylan: ", Dylan)
-    union = Dylan | alice | bob | charlie
-    print("\nAll distinct achievements: ", union)
-    intersection = alice & bob & charlie & Dylan
-    print("\nCommon achievements:", intersection)
+    print("Player Dylan: ", dylan)
+    uni = set.union(dylan, alice, bob, charlie)
+#    union = dylan | alice | bob | charlie
+    print("\nAll distinct achievements: ", uni)
+    inter = set.intersection(dylan, alice, bob, charlie)
+#    intersection = alice & bob & charlie & dylan
+    print("\nCommon achievements:", inter)
+    print("Only Alice has:", uniq_achiev((dylan | bob | charlie), alice))
+    print("Only Bob has:", uniq_achiev((dylan | alice | charlie), bob))
+    print("Only Charlie has:", uniq_achiev((dylan | bob | alice), charlie))
+    print("Only Dylan has:", uniq_achiev((alice | bob | charlie), dylan))
+    print("\nAlic is missing:", no_achiev((dylan | bob | charlie), alice))
+    print("Bob is missing:", no_achiev((dylan | alice | charlie), bob))
+    print("Charlie is missing:", no_achiev((dylan | bob | alice), charlie))
+    print("Dylan is missing:", no_achiev((alice | bob | charlie), dylan))
+
 
 if __name__ == "__main__":
     main()
